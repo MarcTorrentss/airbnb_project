a3d6c4e5dfc105b9ae757ad6b50eb05b7d9a7624
Marc Torrents <122527618+MarcTorrentss@users.noreply.github.com>
2024-05-28 23:57:20 +0200

    Update ny_app.py

--- ny_app.py
+++ ny_app.py
@@ -200,14 +200,9 @@ elif page == "Price predictor":
             if submit_button:
                 input_data = pd.DataFrame([[beds, accom, bath, barrio]], columns=['beds', 'accommodates', 'bathrooms', 'neighbourhood_cleansed']) 
 
-                # 1 - Encode what the user types into numbers using the mapping json.
-                input_data['neighbourhood_cleansed'] = input_data['neighbourhood_cleansed'].replace(encoder)
-        
-                # 2 - Normalise the input data
-                dtest = scaler.transform(input_data)
-
-                # 3 - Make the prediction with the trained model
-                prediction = model.predict(dtest)
-        
-                predicted_price = prediction[-1]  # Generally, the prediction is in the last column.
-                st.write(f"### The predicted price of the accommodation is {predicted_price:.2f} €")
+                st.write("Selected values:")
+                st.write("District:", distrito)
+                st.write("Neighborhood:", barrio)
+                st.write("Beds:", beds)
+                st.write("Accommodates:", accom)
+                st.write("Bathrooms:", bath)
