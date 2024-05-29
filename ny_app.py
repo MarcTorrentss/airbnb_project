6f81dece51509ee2c656a076825cd326455ef51d
Marc Torrents <122527618+MarcTorrentss@users.noreply.github.com>
2024-05-29 22:37:32 +0200

    Update ny_app.py

--- ny_app.py
+++ ny_app.py
@@ -149,15 +149,29 @@ elif page == "Reviews":
         
     st.markdown('A **sentiment analysis** of the reviews has also been carried out. You can see a visualisation of the distribution of sentiment between positive, negative or neutral:')
         
-    # Open html file
+    '''# Open html file
     with open("images/sentimentalanalysis.html", 'r', encoding='utf-8') as SentFile:
     # Read and load into source_code variable
         source_code = SentFile.read()
         print(source_code)
 
     # View content on Streamlit
-    components.html(source_code, height=600)
+    components.html(source_code, height=600)'''
 
+    file_path = "images/sentimentalanalysis.html"
+    if not os.path.isfile(file_path):
+        st.error(f"El archivo {file_path} no existe. Verifica la ruta y el nombre del archivo.")
+    else:
+        try:
+            with open(file_path, 'r', encoding='utf-8') as SentFile:
+                # Leer el contenido del archivo HTML
+                source_code = SentFile.read()
+                st.success("Archivo HTML leído correctamente.")
+            
+                # Mostrar el contenido HTML en Streamlit
+                components.html(source_code, height=600)
+        except Exception as e:
+            st.error(f"Error al leer el archivo: {e}")
 
 
 # PAGE 4-------------------------------------
